9de6d26b06b1266431296983a02935fa4a852a97
matsutani <yushiro.ma2ta2.21@gmail.com>
2017-02-05 13:56:19 +0900

    全てのjs cssを対象にする

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#デフォルトではapplication.jsとapplication.cssしか対象になっていないため、config/initializers/assets.rbに設定を追加し、全ての JavaScript と CSS ファイルを対象にするようにする
+Rails.application.config.assets.precompile += %w(*.js *.css)
+
+#Rails で Views 系の Gem を使っている場合、使用している Gem によっては Precompile でエラーが発生する場合がある。
+# その場合は、上記の設定の代わりに、以下のアンダースコアから始まるファイルを対象外とする設定を使用
+Rails.application.config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*(\.js|\.css)$/
